3a4e107f45f14baf71537d8df481b55503f7ec51
xdh-git <xudanhui123@gmail.com>
2022-11-19 21:37:01 +0800

    modified:   black.py 	modified:   setting.py

--- black.py
+++ black.py
@@ -1,11 +1,20 @@
 import tkinter
+import configparser
 
-root = tkinter.Tk()
-root.overrideredirect(True)
+root = tkinter.Tk() 
+# root.overrideredirect(True)
 root.config(bg="Black")
 length_1 = root.winfo_screenheight()
 width_1 = root.winfo_screenwidth()
-root.geometry(str(width_1)+"x"+str(length_1))
+root.geometry(str(width_1) + "x" + str(length_1))
 root.wm_attributes("-topmost", 1)
-
+ini = configparser.ConfigParser()
+ini.read("./config.ini", encoding='utf-8')
+last = ini.getint('home', 'last')
+rest_label = tkinter.Label(text="Have a rest for " + str(last) + " minute(s)",
+                           bg='black',
+                           fg='white',
+                           font = ("Consolas", 36)
+                           )
+rest_label.place(relx=.5, rely=.5,anchor= tkinter.CENTER)
 root.mainloop()
--- setting.py
+++ setting.py
@@ -6,18 +6,25 @@ from PyQt5.QtWidgets import *
 
 class Main(QMainWindow, ui_setting.Ui_MainWindow):
 
+    def save(self):
+        self.ini.set('home', 'between', str(self.between.value()))
+        print(self.between.value())
+        self.ini.set('home', 'last', str(self.last.value()))
+        print(self.last.value())
+
     def __init__(self):
         QMainWindow.__init__(self)
         ui_setting.Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        ini = configparser.ConfigParser()
-        ini.read("./config.ini", encoding='utf-8')
-        between = ini.getint('home', 'between')
-        last = ini.getint('home', 'last')
+        self.ini = configparser.ConfigParser()
+        self.ini.read("./config.ini", encoding='utf-8')
+        between = self.ini.getint('home', 'between')
+        last = self.ini.getint('home', 'last')
         self.between.setMaximum(1000)
         self.between.setProperty("value", between)
         self.last.setMaximum(30)
         self.last.setProperty("value", last)
+        self.pushButton.clicked.connect(self.save)
 
 
 if __name__ == '__main__':
