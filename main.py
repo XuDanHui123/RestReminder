cff35f8f486aedd6f22084ef5211f58df2ba5e37
xdh-git <xudanhui123@gmail.com>
2022-11-23 19:45:06 +0800

    deleted:    Alarm01.wav 	deleted:    Alarm02.wav 	deleted:    Alarm03.wav 	deleted:    Alarm04.wav 	deleted:    Alarm05.wav 	deleted:    Alarm06.wav 	deleted:    Alarm07.wav 	deleted:    Alarm08.wav 	deleted:    Alarm09.wav 	deleted:    Alarm10.wav 	modified:   config.ini 	new file:   java-coffee-cup-logo--v1_2.ico 	new file:   java-coffee-cup-logo--v1_2.png 	modified:   main.py 	new file:   rest_scr.exe 	renamed:    black.py -> rest_scr.py 	new file:   rest_scr.spec

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from multiprocessing import Process
 import os
 import time
-import tkinter
 import configparser
-import setting
-# import heartrate
-
-# heartrate.trace(browser=True)
 class init():
     def __init__(self):
         self.ini = configparser.ConfigParser();
@@ -15,16 +9,16 @@ class init():
         if(used==0):
             del self.ini
             os.system('setting.exe')
-            self.ini = configparser.ConfigParser();
+            self.ini = configparser.ConfigParser()
             self.ini.read("./config.ini", encoding='utf-8')
             self.ini.set('home', 'used', '1')
             self.ini.write(open("config.ini", "w"))
-        # self.rest()
+        self.rest()
 
     def rest(self):
         while True:
             time.sleep(self.ini.getint('home','between')*60)
-            os.system('black.py')
+            os.system('rest_scr.exe')
             time.sleep(self.ini.getint('home','last')*60)
 
 if __name__ == '__main__':
